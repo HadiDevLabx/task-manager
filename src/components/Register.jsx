bd76f624a3762868e899c233745649466deef4e8
xhadi <81356467+HadiDevLabx@users.noreply.github.com>
2024-06-01 20:55:28 +0400

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -119,9 +119,10 @@ const Register = () => {
                           message: 'Password must be at least 6 characters long',
                         },
                         pattern: {
-                          value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d@$!%*?&amp;]{6,}$/,
-                          message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-                         },
+                          value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$/,
+                          message: 'Password must contain at least one letter, one number, and be at least 6 characters long',
+                        }                        
+                        ,
                       })}
                       error={!!errors.password}
                       helperText={errors.password ? errors.password.message : ''}
